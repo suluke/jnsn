e60299e9b45a6c57a784952d35806d79831aac52
Lukas Böhm <suluke93@gmail.com>
2017-10-24 23:10:56 +0200

    Extend unittest for ast_node_store
    This was because of the recent removal of the ast_node_ref-based
    implementation

--- unittest/ast_test.cc
+++ unittest/ast_test.cc
@@ -30,6 +30,26 @@ TEST(ast_test, node_store) {
   name_checker checker;
   auto res = checker.visit(*node);
   ASSERT_STREQ(res, "module");
+  // stress test container insertion
+  constexpr int mod_count = 5000;
+  constexpr int expr_count = 100;
+  std::vector<module_node *> modules;
+  modules.reserve(mod_count);
+  for (int i = 0; i < mod_count; i++) {
+    modules.emplace_back(store.make_module());
+  }
+  auto *expr = store.make_empty_expr();
+  for (auto *mod : modules) {
+    for (int i = 0; i < expr_count; i++) {
+      mod->exprs.emplace_back(expr);
+    }
+  }
+  for (int i = 0; i < mod_count; i++) {
+    store.make_module();
+  }
+  for (auto *mod : modules) {
+    ASSERT_EQ(mod->exprs.size(), (size_t) expr_count);
+  }
 }
 
 TEST(ast_test, printing) {
