f2aa1a6022be0d8d0c9fe015b24d8107528bf595
Lukas Böhm <suluke93@gmail.com>
2017-11-11 21:38:06 +0100

    Fix unittests

--- unittest/ast_ops_test.cc
+++ unittest/ast_ops_test.cc
@@ -7,22 +7,22 @@ TEST(ast_ops_test, isa) {
   ast_node *ptr;
 #define NODE(NAME, CHILDREN)                                                   \
   {                                                                            \
-    auto node = NAME##_node{};                                                 \
+    auto node = NAME##_node({});                                                 \
     ptr = &node;                                                               \
     ASSERT_TRUE(isa<NAME##_node>(ptr));                                        \
   }
 #define DERIVED(NAME, EXTENDS, CHILDREN) NODE(NAME, CHILDREN)
 #include "parsing/ast.def"
   {
-    auto node = module_node{};
+    auto node = module_node({});
     ASSERT_FALSE(isa<statement_node>(node));
   }
   {
-    auto node = function_expr_node{};
+    auto node = function_expr_node({});
     ASSERT_TRUE(isa<expression_node>(node));
   }
   {
-    auto node = statement_node{};
+    auto node = statement_node({});
     ASSERT_FALSE(isa<expression_node>(node));
   }
 }
