3360adfda777cef44126bd62e67dad640a56e551
Lukas Böhm <suluke93@gmail.com>
2017-11-27 19:10:03 +0100

    Implement add and sub in ir_builder

--- lib/ir/ir_builder.cc
+++ lib/ir/ir_builder.cc
@@ -1,10 +1,13 @@
 #include "parsing/ast_ops.h"
 #include "parsing/ast_walker.h"
+#include "parsing/ir/intrinsics.h"
 #include "parsing/ir/ir_builder.h"
 #include "parsing/ir/module.h"
 #include "parsing/util.h"
+#include <array>
 #include <cstdlib>
 #include <map>
+#include <string>
 
 namespace parsing {
 
@@ -28,17 +31,89 @@ struct ir_builder {
   module *mod;
   ir_builder(ir_context &ctx) : ctx(ctx) {}
   result build(const module_node &ast);
+  basic_block *make_block(function &F) {
+    auto *bb = ctx.make_block();
+    ctx.insert_block_into(F, *bb);
+    return bb;
+  }
   template <class ty> ty *insert_inst(basic_block &bb) {
     assert(mod);
     auto *inst = ctx.make_inst<ty>();
-    ctx.insert_inst_into(&bb, inst);
+    ctx.insert_inst_into(bb, *inst);
     return inst;
   }
   template <class Inst>
   void set_inst_arg(Inst &inst, typename Inst::arguments arg, value &val) {
     ctx.set_inst_arg(inst, arg, val);
   }
+  template <class Iterator>
+  instruction *prepare_call_args(basic_block &IP, Iterator begin,
+                                 Iterator end) {
+    auto *args = insert_inst<alloc_object_inst>(IP);
+    unsigned idx = 0;
+    for (auto It = begin; It != end; ++It) {
+      auto *idx_val = ctx.get_c_str_val(std::to_string(idx));
+      auto *addr = insert_inst<def_prop_inst>(IP);
+      set_inst_arg(*addr, def_prop_inst::arguments::address, *args);
+      set_inst_arg(*addr, def_prop_inst::arguments::prop, *idx_val);
+      auto *store = insert_inst<store_inst>(IP);
+      set_inst_arg(*store, store_inst::arguments::address, *addr);
+      set_inst_arg(*store, store_inst::arguments::value, **It);
+    }
+    return args;
+  }
+  call_inst *cast_to_number(basic_block &IP, value &val) {
+    std::array<value *, 1> args = {&val};
+    auto *to_num = get_intrinsic(intrinsic::to_number);
+    auto *args_list = prepare_call_args(IP, args.begin(), args.end());
+    auto *num = insert_inst<call_inst>(IP);
+    set_inst_arg(*num, call_inst::arguments::callee, *to_num);
+    set_inst_arg(*num, call_inst::arguments::this_val, *ctx.get_undefined());
+    set_inst_arg(*num, call_inst::arguments::arguments, *args_list);
+    return num;
+  }
+  call_inst *cast_to_primitive(basic_block &IP, value &val) {
+    std::array<value *, 1> args = {&val};
+    auto *to_prim = get_intrinsic(intrinsic::to_primitive);
+    auto *args_list = prepare_call_args(IP, args.begin(), args.end());
+    auto *prim = insert_inst<call_inst>(IP);
+    set_inst_arg(*prim, call_inst::arguments::callee, *to_prim);
+    set_inst_arg(*prim, call_inst::arguments::this_val, *ctx.get_undefined());
+    set_inst_arg(*prim, call_inst::arguments::arguments, *args_list);
+    return prim;
+  }
+  call_inst *cast_to_string(basic_block &IP, value &val) {
+    std::array<value *, 1> args = {&val};
+    auto *to_str = get_intrinsic(intrinsic::to_string);
+    auto *args_list = prepare_call_args(IP, args.begin(), args.end());
+    auto *str = insert_inst<call_inst>(IP);
+    set_inst_arg(*str, call_inst::arguments::callee, *to_str);
+    set_inst_arg(*str, call_inst::arguments::this_val, *ctx.get_undefined());
+    set_inst_arg(*str, call_inst::arguments::arguments, *args_list);
+    return str;
+  }
+  call_inst *test_is_string(basic_block &IP, value &val) {
+    std::array<value *, 1> args = {&val};
+    auto *is_str = get_intrinsic(intrinsic::is_string);
+    auto *args_list = prepare_call_args(IP, args.begin(), args.end());
+    auto *res = insert_inst<call_inst>(IP);
+    set_inst_arg(*res, call_inst::arguments::callee, *is_str);
+    set_inst_arg(*res, call_inst::arguments::this_val, *ctx.get_undefined());
+    set_inst_arg(*res, call_inst::arguments::arguments, *args_list);
+    return res;
+  }
+  call_inst *concat_strings(basic_block &IP, value &lhs, value &rhs) {
+    std::array<value *, 2> args = {&lhs, &rhs};
+    auto *concat = get_intrinsic(intrinsic::concat);
+    auto *args_list = prepare_call_args(IP, args.begin(), args.end());
+    auto *res = insert_inst<call_inst>(IP);
+    set_inst_arg(*res, call_inst::arguments::callee, *concat);
+    set_inst_arg(*res, call_inst::arguments::this_val, *ctx.get_undefined());
+    set_inst_arg(*res, call_inst::arguments::arguments, *args_list);
+    return res;
+  }
   std::string get_unique_id(value &v) { return ctx.get_unique_id(v); }
+  function *get_intrinsic(intrinsic i) { return ctx.get_intrinsic(i); }
 };
 
 using inst_result = std::variant<ir_error, value *>;
@@ -201,26 +276,121 @@ struct inst_creator : public const_ast_node_visitor<inst_result> {
   result accept(const bin_op_expr_node &) override {
     return ir_error{"Encountered abstract class bin_op_expr_node", {}};
   }
-  // arithmetic binops
+    // arithmetic binops
+#define LOAD_BINOP_ARGS(lhs, rhs)                                              \
+  do {                                                                         \
+    auto lhs_err = visit(*node.lhs);                                           \
+    if (std::holds_alternative<ir_error>(lhs_err)) {                           \
+      return lhs;                                                              \
+    }                                                                          \
+    auto rhs_err = visit(*node.rhs);                                           \
+    if (std::holds_alternative<ir_error>(rhs_err)) {                           \
+      return rhs;                                                              \
+    }                                                                          \
+    lhs = std::get<value *>(lhs_err);                                          \
+    rhs = std::get<value *>(rhs_err);                                          \
+  } while (false)
+
   result accept(const add_node &node) override {
-    auto lhs = visit(*node.lhs);
-    if (std::holds_alternative<ir_error>(lhs)) {
-      return lhs;
+    // See dmitripavlutin.com/javascriptss-addition-operator-demystified/
+    assert(IP);
+    auto &start = *IP;
+    assert(start.get_parent());
+    auto &F = *IP->get_parent();
+
+    value *lhs, *rhs;
+    LOAD_BINOP_ARGS(lhs, rhs);
+
+    // convert lhs to primitive
+    call_inst *lhs_prim = builder.cast_to_primitive(start, *lhs);
+    call_inst *rhs_prim = builder.cast_to_primitive(start, *rhs);
+    // see if lhs primitive is string
+    call_inst *lhs_is_str = builder.test_is_string(start, *lhs_prim);
+    call_inst *rhs_is_str = builder.test_is_string(start, *rhs_prim);
+
+    // define some branch targets for later use
+    auto *on_concat = builder.make_block(F);
+    auto *on_add = builder.make_block(F);
+    auto *val_unification = builder.make_block(F);
+
+    // dispatch either to concatenation or addition
+    {
+      // test if either one is string
+      auto *lhs_or_rhs_str = builder.insert_inst<log_or_inst>(*IP);
+      builder.set_inst_arg(*lhs_or_rhs_str, log_or_inst::arguments::lhs,
+                           *lhs_is_str);
+      builder.set_inst_arg(*lhs_or_rhs_str, log_or_inst::arguments::rhs,
+                           *rhs_is_str);
+
+      auto *br = builder.insert_inst<cbr_inst>(*IP);
+      builder.set_inst_arg(*br, cbr_inst::arguments::cond, *lhs_or_rhs_str);
+      builder.set_inst_arg(*br, cbr_inst::arguments::true_target, *on_concat);
+      builder.set_inst_arg(*br, cbr_inst::arguments::false_target, *on_add);
     }
-    auto rhs = visit(*node.rhs);
-    if (std::holds_alternative<ir_error>(rhs)) {
-      return rhs;
+
+    // codegen for concat
+    call_inst *concat = nullptr;
+    {
+      auto *lhs_str = builder.cast_to_string(*on_concat, *lhs_prim);
+      auto *rhs_str = builder.cast_to_string(*on_concat, *rhs_prim);
+
+      concat = builder.concat_strings(*on_concat, *lhs_str, *rhs_str);
+
+      auto *concat_br = builder.insert_inst<br_inst>(*on_concat);
+      builder.set_inst_arg(*concat_br, br_inst::arguments::target, *val_unification);
     }
-    // FIXME arguments should be cast to number first
-    auto *add = builder.insert_inst<add_inst>(*IP);
-    builder.set_inst_arg(*add, add_inst::arguments::lhs,
-                         *std::get<value *>(lhs));
-    builder.set_inst_arg(*add, add_inst::arguments::rhs,
-                         *std::get<value *>(rhs));
-    return add;
+
+    // codegen for addition
+    add_inst *add = nullptr;
+    {
+      auto *lhs_num = builder.cast_to_number(*on_add, *lhs);
+      auto *rhs_num = builder.cast_to_number(*on_add, *rhs);
+      
+      add = builder.insert_inst<add_inst>(*on_add);
+      builder.set_inst_arg(*add, add_inst::arguments::lhs, *lhs_num);
+      builder.set_inst_arg(*add, add_inst::arguments::rhs, *rhs_num);
+
+      auto *add_br = builder.insert_inst<br_inst>(*on_add);
+      builder.set_inst_arg(*add_br, br_inst::arguments::target, *val_unification);
+    }
+
+    // now unify results
+    auto *val = builder.insert_inst<phi_inst>(*val_unification);
+    builder.set_inst_arg(*val, phi_inst::arguments::in1, *on_concat);
+    builder.set_inst_arg(*val, phi_inst::arguments::val2, *concat);
+    builder.set_inst_arg(*val, phi_inst::arguments::in2, *on_add);
+    builder.set_inst_arg(*val, phi_inst::arguments::val2, *add);
+
+    IP = val_unification;
+    return val;
   }
   result accept(const subtract_node &node) override {
-    return ir_error{"Not implemented", {}};
+    value *lhs, *rhs;
+    LOAD_BINOP_ARGS(lhs, rhs);
+    std::array<value *, 1> args;
+    auto *to_num = builder.get_intrinsic(intrinsic::to_number);
+    // turn lhs into a number
+    args[0] = lhs;
+    auto *lhs_args = builder.prepare_call_args(*IP, args.begin(), args.end());
+    auto *lhs_num = builder.insert_inst<call_inst>(*IP);
+    builder.set_inst_arg(*lhs_num, call_inst::arguments::callee, *to_num);
+    builder.set_inst_arg(*lhs_num, call_inst::arguments::this_val,
+                         *builder.ctx.get_undefined());
+    builder.set_inst_arg(*lhs_num, call_inst::arguments::arguments, *lhs_args);
+    // turn rhs into a number
+    args[0] = rhs;
+    auto *rhs_args = builder.prepare_call_args(*IP, args.begin(), args.end());
+    auto *rhs_num = builder.insert_inst<call_inst>(*IP);
+    builder.set_inst_arg(*rhs_num, call_inst::arguments::callee, *to_num);
+    builder.set_inst_arg(*rhs_num, call_inst::arguments::this_val,
+                         *builder.ctx.get_undefined());
+    builder.set_inst_arg(*rhs_num, call_inst::arguments::arguments, *rhs_args);
+
+    //
+    auto *res = builder.insert_inst<sub_inst>(*IP);
+    builder.set_inst_arg(*res, sub_inst::arguments::lhs, *lhs_num);
+    builder.set_inst_arg(*res, sub_inst::arguments::rhs, *rhs_num);
+    return res;
   }
   result accept(const multiply_node &node) override {
     return ir_error{"Not implemented", {}};
@@ -468,8 +638,8 @@ ir_builder::result ir_builder::build(const module_node &ast) {
   this->mod = mod.get();
   ast_ir_mappings mappings;
   auto *entry = ctx.make_function();
-  ctx.insert_function_into(mod.get(), entry);
-  ctx.insert_block_into(entry, ctx.make_block());
+  ctx.insert_function_into(*mod, *entry);
+  ctx.insert_block_into(*entry, *ctx.make_block());
   entry->set_name("__module_entry__");
   hoisted_codegen(*this, *entry->get_entry()).visit(ast);
   auto res = inst_creator(*this, entry->get_entry()).visit(ast);
@@ -486,11 +656,11 @@ ir_builder::result ir_builder::build(const module_node &ast) {
       const auto as_stmt = static_cast<const function_stmt_node *>(ast_func);
       F->set_name(to_str(as_stmt->name));
     }
-    ctx.insert_block_into(F, ctx.make_block());
+    ctx.insert_block_into(*F, *ctx.make_block());
     mappings.funcs.emplace(ast_func, F);
     const auto *body = get_function_body(ast_func);
     hoisted_codegen(*this, *F->get_entry()).visit(*body);
-    ctx.insert_function_into(mod.get(), F);
+    ctx.insert_function_into(*mod, *F);
     auto res = inst_creator(*this, F->get_entry()).visit(*body);
     if (std::holds_alternative<ir_error>(res)) {
       auto &err = std::get<ir_error>(res);
