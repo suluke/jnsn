9285af209654702df905a84f0be1ed0ef1b47e58
Lukas Böhm <suluke93@gmail.com>
2017-11-29 00:03:25 +0100

    Fix ir unittest

--- unittest/ir_test.cc
+++ unittest/ir_test.cc
@@ -8,7 +8,7 @@ TEST(ir_test, type_isa) {
   ASSERT_TRUE(isa<value_type>(addr_type::create()));  // inheritance
   ASSERT_TRUE(isa<value_type>(value_type::create())); // reflexive
   ASSERT_FALSE(isa<addr_type>(bool_type::create()));  // same pos in hierarchy
-  ASSERT_FALSE(isa<register_type>(heap_value_type::create()));
+  ASSERT_FALSE(isa<register_type>(object_type::create()));
 }
 TEST(ir_test, value_isa) {
   ir_context ctx;
@@ -19,7 +19,7 @@ TEST(ir_test, value_isa) {
   ASSERT_TRUE(isa<add_inst>(a)); // reflexive
   ASSERT_FALSE(isa<sub_inst>(a));  // same pos in hierarchy
   ASSERT_TRUE(isa<value>(str));
-  ASSERT_TRUE(isa<constant>(str));
+  ASSERT_TRUE(isa<global_value>(str));
   ASSERT_TRUE(isa<c_str_val>(str));
   ASSERT_TRUE(isa<value>(num));
   ASSERT_TRUE(isa<constant>(num));
