c5f09f959089c8228b2852d6aa40ed8e334ebcef
Lukas Böhm <suluke93@gmail.com>
2017-12-10 13:13:45 +0100

    Fix ir unittest

--- unittest/ir_test.cc
+++ unittest/ir_test.cc
@@ -1,4 +1,4 @@
-#include "jnsn/ir/ir_context.h"
+#include "jnsn/ir/module.h"
 #include "gtest/gtest.h"
 #include <sstream>
 
@@ -12,15 +12,17 @@ TEST(ir_test, type_isa) {
 }
 TEST(ir_test, value_isa) {
   ir_context ctx;
-  auto &str = *ctx.get_c_str_val("test");
+  module mod(ctx);
+  auto &str = *mod.get_str_val("test");
   auto &num = *ctx.get_c_num_val(1.);
   add_inst a{ctx};
-  ASSERT_TRUE(isa<value>(a));  // inheritance
-  ASSERT_TRUE(isa<add_inst>(a)); // reflexive
-  ASSERT_FALSE(isa<sub_inst>(a));  // same pos in hierarchy
+  ASSERT_TRUE(isa<value>(a));     // inheritance
+  ASSERT_TRUE(isa<add_inst>(a));  // reflexive
+  ASSERT_FALSE(isa<sub_inst>(a)); // same pos in hierarchy
   ASSERT_TRUE(isa<value>(str));
   ASSERT_TRUE(isa<global_value>(str));
   ASSERT_TRUE(isa<c_str_val>(str));
+  ASSERT_FALSE(isa<constant>(str));
   ASSERT_TRUE(isa<value>(num));
   ASSERT_TRUE(isa<constant>(num));
   ASSERT_TRUE(isa<c_num_val>(num));
